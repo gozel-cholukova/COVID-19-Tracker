a49f8fbfb468421e18a1d5e57e3fa529b6ed251e
Gozel Cholukova <gozelr@mail.ru>
2021-03-28 02:51:47 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,201 @@
+import React, { useEffect, useState } from 'react';
+import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/core";
+import InfoBox from './InfoBox';
+import './App.css';
+import Map from './Map';
+
+
+function App() {
+  const [continents, setContinents] =  useState([]);
+  const [continent, setContinent] =  useState('worldwide');
+  const [continentInfo, setContinentInfo] = useState();
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState();
+  // STATE = how to write a variable in REACT <<<<<<<<
+
+  // https://disease.sh/v3/covid-19/continents
+  
+  //USEEFFECT = Runs a piece of code based on a given condition
+
+  useEffect(() => {
+    // The code inside here will run once when the component loads and not again after
+
+    // asyns -> send a request, wait for it, do something with input
+    const getContinentsData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/continents")
+      .then((response) => response.json())
+      .then((data) => {
+        const continents = data.map((continent) => ({
+            name: continent.continent,
+            value: continent.continentInfo,
+          }));
+          setContinents(continents);          
+      });
+    };
+    getContinentsData();
+
+
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name: country.country, //United States, United Kingdom....
+            value: country.countryInfo.iso2 //USA, UK, RU
+          }));
+          setCountries(countries);
+      });
+    };
+    getCountriesData();
+  }, []);
+
+  // useEffect(() => {
+  //   const getCountriesData = async () => {
+  //     await fetch("https://disease.sh/v3/covid-19/countries")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const countries = data.map((country) => ({
+  //           name: country.country, //United States, United Kingdom....
+  //           value: country.countryInfo.iso2 //USA, UK, RU
+  //         }));
+
+  //         setCountries(country);
+  //     });
+  //   };
+
+  //   getCountriesData();
+  // }, []);
+
+  const onContinentChange = async (e) => {
+    const continentCode = e.target.value;
+
+    setContinent(continentCode);
+
+    const url = continentCode === "worldwide"
+      ? "https://disease.sh/v3/covid-19/all"
+      : `https://disease.sh/v3/covid-19/continents${continentCode}`;
+    //https://disease.sh/v3/covid-19/continents
+  }
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+
+    setCountry(countryCode);
+
+    const url = countryCode === "worldwide"
+      ? "https://disease.sh/v3/covid-19/all"
+      : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+      await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+            name: country.country, //United States, United Kingdom....
+            value: country.countryInfo.iso2 //USA, UK, RU
+          }));
+  }
+  
+  return (
+    <>
+      <div className="app">
+        {/* Header */}
+        <div className="app__left">
+          <div className="app__header">
+          <h1>COVID-19  TRACK by Continents</h1>
+            <FormControl className="app__dropdown">
+              <Select
+              variant="outlined"
+              onChange={onContinentChange}
+              value={continent}
+              >
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {/* Loop through all the continents and show a drop down list of the options */}
+                {continents.map((continent) => (
+                  <MenuItem value={continent.value}>
+                    {continent.name}
+                  </MenuItem>
+                ))}             
+                {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+                <MenuItem value="worldwide">Europe</MenuItem>
+                <MenuItem value="worldwide">Asia</MenuItem>
+                <MenuItem value="worldwide">Africa</MenuItem>
+                <MenuItem value="worldwide">North America</MenuItem>
+                <MenuItem value="worldwide">South America</MenuItem> */}
+              </Select>
+            </FormControl>
+          </div>
+
+          <div className="app__stats">
+            <InfoBox title="Coronavirus Cases" cases={111} total={2000} />
+            <InfoBox title="Recovered" cases={111} total={2000} />
+            <InfoBox title="Deaths" cases={111} total={2000} />
+          </div>
+
+          <Map />
+        </div>
+
+        <Card className="app__right">
+          <CardContent>
+            <h3>Live Cases by Continent</h3>
+            {/* Table */}
+            <h3>Worldwide new cases</h3>
+            {/* Graph */}
+          </CardContent>
+        </Card>
+      </div>
+
+      
+      <div className="app1">
+        <div className="app__contiLeft">
+          <CardContent>
+            <h3>Live Cases by Country</h3>
+            {/* Table */}
+            <h3>Worldwide new cases</h3>
+            {/* Graph */}
+          </CardContent>       
+        </div>
+
+        <div className="app__contiRight">
+          <div className="app__header">        
+            <h1>COVID-19  TRACK by Country</h1>
+            <FormControl className="app__dropdown">
+              <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+              >
+                {/* Loop through all the continents and show a drop down list of the options */}
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {countries.map((country) => (
+                  <MenuItem value={country.value}>
+                    {country.name}
+                  </MenuItem>
+                ))}              
+              </Select>
+            </FormControl>
+          </div>
+
+          {/* Title */} 
+          <div className="app__stats">
+            <InfoBox title="Coronavirus Cases" cases={111} total={2000} />
+            <InfoBox title="Recovered" cases={111} total={2000} />
+            <InfoBox title="Deaths" cases={111} total={2000} />
+          </div>
+          <Map /> 
+        </div>
+
+       
+      </div>
+    </> 
+      
+
+            
+    
+    
+    
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbJNhDdgZEAMT4Ly81kCWlPvuZRZbY0k8",
+  authDomain: "covid-19-tracker-97cb4.firebaseapp.com",
+  databaseURL: "https://covid-19-tracker-97cb4-default-rtdb.firebaseio.com",
+  projectId: "covid-19-tracker-97cb4",
+  storageBucket: "covid-19-tracker-97cb4.appspot.com",
+  messagingSenderId: "91853932425",
+  appId: "1:91853932425:web:1c52a9ae1bc387b967c358"
+};
