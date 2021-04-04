1752f58b05338e4e83289d2ca7e6e1a471c805d3
Gozel Cholukova <gozelr@mail.ru>
2021-04-04 23:44:32 +0500

    Add files via upload

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
-import {Line} from "react-chartjs2";
+import React, { useEffect, useState } from 'react';
+import { Line } from "react-chartjs-2";
 import "./LineGraph.css";
 
 function LineGraph() {
   const [data, setData] = useState({});
+  
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
+    .then((response) => response.json())
+    .then ((data) => {
+
+    });
+  }, []);
+
+  const buildChartData = data => {
+    const chartData = [];
+    let lastDataPoint;
+  }
   return (
     <div>
-      <Line data options />
+      <h1>Graph</h1>
+      {/* <Line 
+        data 
+        options 
+      /> */}
     </div>
   );
 }
