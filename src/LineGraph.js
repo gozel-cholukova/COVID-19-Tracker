38dd8c637c415bcecc3a7dda99b89a7d45aca4a0
Gozel Cholukova <gozelr@mail.ru>
2021-04-07 04:22:34 +0500

    Add files via upload

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from "react-chartjs-2";
 import numeral from "numeral";
-import "./LineGraph.css";
 
 const options = {
   legend: {
@@ -44,58 +43,60 @@ const options = {
         },
       },
     ],
-  }
-}
+  },
+};
 
-function LineGraph() {
-  const [data, setData] = useState({});
-
-  const buildChartData = (data, casesType = "cases") => {
-    let chartData = [];
-    let lastDataPoint;
-    for (let date in data.cases) {
-      if (lastDataPoint) {
-        let newDataPoint = {
-          x: date,
-          y: data[casesType][date] - lastDataPoint,
-        };
-        chartData.push(newDataPoint);
-      }
-      lastDataPoint = data[casesType][date];
+const buildChartData = (data, casesType) => {
+  let chartData = [];
+  let lastDataPoint;
+  for (let date in data.cases) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint,
+      };
+      chartData.push(newDataPoint);
     }
-    return chartData;
-  };
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
 
+function LineGraph({ casesType = "cases" }) {
+  const [data, setData] = useState({});
   
   useEffect(() => {
     const fetchData = async () => {
       await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=120")
-      .then((response) => response.json())
+      .then((response) => {
+        return response.json();
+      })
       .then((data) => {
-        let chartData = buildChartData(data, "cases");
+        let chartData = buildChartData(data, casesType);
         setData(chartData);
       });
-    }   
-    fetchData();
-  }, []);
+    };
 
-  
+    fetchData();
+  }, [casesType]);
 
   return (
     <div>
-      <h1>Graph</h1>
-      <Line 
-        options={options}
-        data={{
-          datasets: [
-            {
-              backgroundColor:"rgba(204, 16, 52, 0.5)",
-              borderColor:"#CC1034",
-              data: data,
-            },
-          ],
-        }}
-      />
+      {data?.length > 0 && (
+        <Line 
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
+      
     </div>
   );
 }
