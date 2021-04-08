9706a226879448d9afd1d593955dc51633c444df
Gozel Cholukova <gozelr@mail.ru>
2021-04-08 23:51:58 +0500

    Add files via upload

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
 import './Map.css';
 
-function Map() {
+function Map({ center, zoom }) {
   return (
     <div className="map">
-      <LeafletMap>
+      <h1>Map</h1>
+      <LeafletMap center={center} zoom={zoom}>
         <TileLayer 
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">
-          OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-      </LeafletMap>
-      
+      </LeafletMap>      
     </div>
   );
 }
