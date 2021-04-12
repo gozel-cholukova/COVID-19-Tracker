8253ededf01887a5e1e65bdbaf1543b5d363c066
Gozel Cholukova <gozelr@mail.ru>
2021-04-12 23:48:00 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,18 +5,28 @@ import './App.css';
 import Map from './Map';
 import Table from "./Table";
 import Tabel from "./Tabel";
-import { sortData } from "./utl";
+import { sortData, prettyPrintStat } from "./utl";
 import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [continents, setContinents] =  useState([]);
   const [continent, setContinent] =  useState("worldwide");
   const [continentInfo, setContinentInfo] = useState({});
+  const [mapContinents, setMapContinents] = useState([]);
+  const [pamCenter, setPamCenter] = useState([31.67682, -146.47074]);
   
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState([34.80746, -40.4796]);
+  const [mapZoom, setMapZoom] = useState(3);
+  //3 is basically zoomout enough to the point where u can see entry map
+  const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+  
+
   // STATE = how to write a variable in REACT <<<<<<<<
 
   // https://disease.sh/v3/covid-19/continents
@@ -47,11 +57,12 @@ function App() {
       .then((data) => {
         const continents = data.map((continent) => ({
             name: continent.continent,
-            value: continent.continentInfo
+            value: continent.continentInfo,
           }));
 
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapContinents(data);
           setContinents(continents);          
       });
     };
@@ -64,11 +75,12 @@ function App() {
         .then((data) => {
           const countries = data.map((country) => ({
               name: country.country, //United States, United Kingdom....
-              value: country.countryInfo.iso2 //USA, UK, RU
+              value: country.countryInfo.iso2, //USA, UK, RU
             }));
 
             const sortedData = sortData(data);
             setTableData(sortedData);
+            setMapCountries(data);
             setCountries(countries);
         });
     };
@@ -89,6 +101,9 @@ function App() {
     .then((data) => {      
       setContinent(continentCode);
       setContinentInfo(data);
+
+      setPamCenter([data.continentInfo.lat, data.continentInfo.long]);
+      setMapZoom(4);
     })
   };
 
@@ -105,71 +120,16 @@ function App() {
     .then((data) => {
       setCountry(countryCode);
       setCountryInfo(data);
-    })
+
+      countryCode === "worldwide"
+        ? setMapCenter([34.80746, -40.4796])
+        : setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+      setMapZoom(4);
+    });
   };
   
   return (
     <>
-      <div className="app">
-        {/* Header */}
-        <div className="app__left">
-          <div className="app__header">
-          <h1>COVID-19  TRACK by Continents</h1>
-            <FormControl className="app__dropdown">
-              <Select
-              variant="outlined"
-              onChange={onContinentChange}
-              value={continent}
-              >
-                <MenuItem value="worldwide">Worldwide</MenuItem>
-                {/* Loop through all the continents and show a drop down list of the options */}
-                {continents.map((continent) => (
-                  <MenuItem value={continent.value}>
-                    {continent.name}
-                  </MenuItem>
-                ))}             
-                {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-                <MenuItem value="worldwide">Europe</MenuItem>
-                <MenuItem value="worldwide">Asia</MenuItem>
-                <MenuItem value="worldwide">Africa</MenuItem>
-                <MenuItem value="worldwide">North America</MenuItem>
-                <MenuItem value="worldwide">South America</MenuItem> */}
-              </Select>
-            </FormControl>
-          </div>
-
-          <div className="app__stats">
-            <InfoBox 
-              title="Coronavirus Cases" 
-              cases={continentInfo.todayCases} 
-              total={continentInfo.cases} 
-            />
-            <InfoBox 
-              title="Recovered" 
-              cases={continentInfo.todayRecovered} 
-              total={continentInfo.recovered} 
-            />
-            <InfoBox 
-              title="Deaths" 
-              cases={continentInfo.todayDeaths} 
-              total={continentInfo.deaths} 
-            />
-          </div>
-
-          <Map />
-        </div>
-
-        <Card className="app__right">
-          <CardContent>
-            <h3>Live Cases by Continent</h3>
-            <Tabel continents={tableData} />
-            <h3>Worldwide new cases</h3>
-            {/* Graph */}
-          </CardContent>
-        </Card>
-      </div>
-
-      
       <div className="app1">
         <div className="app__contiLeft">
           <CardContent>
@@ -203,32 +163,100 @@ function App() {
           {/* Title */} 
           <div className="app__stats">
             <InfoBox 
+              onClick={e => setCasesType("cases")}
               title="Coronavirus Cases" 
-              cases={countryInfo.todayCases} 
-              total={countryInfo.cases} 
+              cases={prettyPrintStat(countryInfo.todayCases)} 
+              total={prettyPrintStat(countryInfo.cases)} 
             />
             <InfoBox 
+              onClick={e => setCasesType("recovered")}
               title="Recovered" 
-              cases={countryInfo.todayRecovered} 
-              total={countryInfo.recovered} 
+              cases={prettyPrintStat(countryInfo.todayRecovered)} 
+              total={prettyPrintStat(countryInfo.recovered)} 
             />
             <InfoBox 
+              onClick={e => setCasesType("deaths")}
               title="Deaths" 
-              cases={countryInfo.todayDeaths} 
-              total={countryInfo.deaths} 
+              cases={prettyPrintStat(countryInfo.todayDeaths)} 
+              total={prettyPrintStat(countryInfo.deaths)} 
             />
           </div>
 
-          <Map /> 
+          <Map 
+            casesType={casesType}
+            countries={mapCountries}
+            center={mapCenter} 
+            zoom={mapZoom}
+          /> 
+        </div>       
+      </div>
+
+      <div className="app">
+        {/* Header */}
+        <div className="app__left">
+          <div className="app__header">
+          <h1>COVID-19  TRACK by Continents</h1>
+            <FormControl className="app__dropdown">
+              <Select
+              variant="outlined"
+              onChange={onContinentChange}
+              value={continent}
+              >
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {/* Loop through all the continents and show a drop down list of the options */}
+                {continents.map((continent) => (
+                  <MenuItem value={continent.value}>
+                    {continent.name}
+                  </MenuItem>
+                ))}             
+                {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+                <MenuItem value="worldwide">Europe</MenuItem>
+                <MenuItem value="worldwide">Asia</MenuItem>
+                <MenuItem value="worldwide">Africa</MenuItem>
+                <MenuItem value="worldwide">North America</MenuItem>
+                <MenuItem value="worldwide">South America</MenuItem> */}
+              </Select>
+            </FormControl>
+          </div>
+
+          <div className="app__stats">
+            <InfoBox 
+              onClick={(e) => setCasesType("cases")}
+              title="Coronavirus Cases" 
+              cases={prettyPrintStat(continentInfo.todayCases)} 
+              total={prettyPrintStat(continentInfo.cases)} 
+            />
+            <InfoBox 
+              onClick={(e) => setCasesType("recovered")}
+              title="Recovered" 
+              cases={prettyPrintStat(continentInfo.todayRecovered)} 
+              total={prettyPrintStat(continentInfo.recovered)} 
+            />
+            <InfoBox 
+              onClick={(e) => setCasesType("deaths")}
+              title="Deaths" 
+              cases={prettyPrintStat(continentInfo.todayDeaths)} 
+              total={prettyPrintStat(continentInfo.deaths)} 
+            />
+          </div>
+
+          <Map 
+            continents={mapContinents}
+            center={pamCenter}
+            zoom={mapZoom}
+          />
         </div>
 
-       
+        <Card className="app__right">
+          <CardContent>
+            <h3>Live Cases by Continent</h3>
+            <Tabel continents={tableData} />
+            <h3>Worldwide new cases</h3>
+            <LineGraph />
+          </CardContent>
+        </Card>
       </div>
-    </> 
-      
-
-            
-    
+    </>   
     
     
   );
